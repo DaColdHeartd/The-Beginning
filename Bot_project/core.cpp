fcaf0fbdd04ad4850748e033c56accdd4071edf4
DaColdHeartd <68741411+DaColdHeartd@users.noreply.github.com>
2022-08-05 21:57:44 +0200

    Update core.cpp

--- Bot_project/core.cpp
+++ Bot_project/core.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
 #include <string>
+#include <ctime>
+#include <chrono>
+#include <ratio>
+
 
 using namespace std;
 
 
 bool prompt(const string& user_name )
 {
+    using namespace chrono;
+
     //declaring the variables locally.
     string user_input, answer;
     string greetings[] = {"hello", "hi", "greetings", "how are you", "wussup"};
+    string date_inputs[] = {"date today", "date tomorrow", "date++"};
+    string time_inputs[] {"time now"}; // Might add more later.
+
+    duration<int,std::ratio<60*60*24> > one_day (1); // making a ratio between time values.
+
+    system_clock::time_point today = system_clock::now(); //Defining today and tomorrow.
+    system_clock::time_point tomorrow = today + one_day;
+
+    time_t tt;
+
 
     cout << "You: ";
     getline(cin, user_input); // using *getline so it takes in strings with spaces.
@@ -17,6 +33,8 @@ bool prompt(const string& user_name )
     //I changed the syntax so I can understand it even more.
     for(int i = 0; i < user_input.length(); i++ )
         user_input[i] = tolower(user_input[i]); //finally used std::tolower :)
+
+
 
     //if statement to answer only to specific strings
     if (user_input.find(greetings[0]) != string::npos)
@@ -39,6 +57,20 @@ bool prompt(const string& user_name )
     {
         answer = "Wussup homie.";
     }
+    else if(user_input.find(date_inputs[0]) != string::npos || user_input.find(time_inputs[0]) != string::npos)
+    {
+        tt = system_clock::to_time_t (today);
+        answer = ctime(&tt);
+    }
+    else if (user_input.find(date_inputs[1]) != string::npos || user_input.find(date_inputs[2]) != string::npos)
+    {
+        tt = system_clock::to_time_t (tomorrow);
+        answer = ctime(&tt);
+    }
+    else if (user_input == "math")
+    {
+      mathematics();
+    }
     else if (user_input == "exit")
     {
         return false;
@@ -50,7 +82,7 @@ bool prompt(const string& user_name )
 
     cout << "Bot: " << answer << "\n" ;
 
-    return true;
+return true;
 
 }
 
@@ -72,4 +104,3 @@ int main()
 
     return 0;
 }
-
